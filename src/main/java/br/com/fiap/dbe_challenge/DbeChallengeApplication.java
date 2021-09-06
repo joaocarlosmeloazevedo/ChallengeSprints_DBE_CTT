1c3db595b03a947c518989c0be0465c8350bdece
Henrique S <rick.sartal@hotmail.com>
2021-09-05 23:08:02 -0300

    Projeto base

---
+++ src/main/java/br/com/fiap/dbe_challenge/DbeChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.dbe_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/dbe_challenge/DbeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.dbe_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
