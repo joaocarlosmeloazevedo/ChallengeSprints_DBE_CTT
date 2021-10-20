39b3b43e4c80609846926a5dbbda0e0cde28d718
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2021-10-19 21:58:14 -0300

    Pasta jenkins e Pom alterado.

--- src/main/java/br/com/fiap/dbe_challenge/DbeChallengeApplication.java
+++ src/main/java/br/com/fiap/dbe_challenge/DbeChallengeApplication.java
@@ -2,14 +2,21 @@ package br.com.fiap.dbe_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class DbeChallengeApplication {
+public class DbeChallengeApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbeChallengeApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(DbeChallengeApplication.class);
+	  }
 
 }
